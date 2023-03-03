967b26f62306766bf0595e9057b97d217651cd99
Шамсутдинов Артем Русланович <pavlovsckikh@yandex.ru>
2023-03-03 17:33:32 +0400

    remove dao

--- src/main/java/com/shamsutdinov/springboot_3_1_2/service/UserServiceImp.java
+++ src/main/java/com/shamsutdinov/springboot_3_1_2/service/UserServiceImp.java
@@ -1,42 +1,35 @@
 package com.shamsutdinov.springboot_3_1_2.service;
 
-import com.shamsutdinov.springboot_3_1_2.dao.UserDao;
 import com.shamsutdinov.springboot_3_1_2.model.User;
 import com.shamsutdinov.springboot_3_1_2.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class UserServiceImp implements UserService {
-    private final UserDao userDao;
     private final UserRepository userRepository;
 
-    public UserServiceImp(UserDao userDao, UserRepository userRepository) {
-        this.userDao = userDao;
+    public UserServiceImp( UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     @Transactional
     public void save(User user) {
-        //userDao.save(user);
         userRepository.save(user);
     }
 
     @Override
     @Transactional
     public List<User> getUsers() {
-        // return userDao.getUsers();
         return userRepository.findAll();
     }
 
     @Override
     @Transactional
     public User getUserById(Long id) {
-        //return userDao.getUserById(id);
         return userRepository.findById(id).orElse(null);
     }
 
@@ -45,14 +38,11 @@ public class UserServiceImp implements UserService {
     public void update(Long id, User user) {
         user.setId(id);
         userRepository.save(user);
-        // userDao.update(id, user);
     }
 
     @Override
     @Transactional
     public void delete(Long id) {
-        //userDao.delete(id);
         userRepository.deleteById(id);
-
     }
 }
