8d2c3ae9efb8214d3a6307a76af6b431dce224bb
Шамсутдинов Артем Русланович <pavlovsckikh@yandex.ru>
2023-03-04 12:33:01 +0400

    fix imports

--- src/main/java/com/shamsutdinov/springboot_3_1_2/controller/UserController.java
+++ src/main/java/com/shamsutdinov/springboot_3_1_2/controller/UserController.java
@@ -4,7 +4,14 @@ import com.shamsutdinov.springboot_3_1_2.model.User;
 import com.shamsutdinov.springboot_3_1_2.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping(value = "/")
 public class UserController {
